1713c4810fea62523ab332af02b9fd323d7a1c94
lambo <1509368098@qq.com>
2016-11-16 11:28:41 +0800

    --

--- app/src/main/java/com/example/lambo/net/BaseNet.java
+++ app/src/main/java/com/example/lambo/net/BaseNet.java
@@ -14,6 +14,7 @@ public abstract class BaseNet {
     public static void setContext(Context _context) {
         context = _context;
         if (queue != null){
+
             queue = Volley.newRequestQueue(context);
         }
     }
